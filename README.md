947d2a0ec41d29bcd1d83166a842875e47c19834
hahnmichaelf <hahn.michael.f@gmail.com>
2018-09-16 19:53:47 -0400

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # stats
+
+
+#### To use:
+
+* In your terminal, run `git clone https://github.com/hahnmichaelf/stats.git`
+* Cd to the new directory with `cd stats`
+* Create a `config.js` file and copy-paste the following code into it:
+```
+module.exports = {
+    'user': 'mycryptohq',
+    'repo': 'mycrypto'
+}
+```
+* Replace the `user` and `repo` fields with the github repo/user that you are wanting to assess.
+* Then run `npm install` and after it has completed run `npm test`
+
+After this, the data will have been pulled and output to data.yaml
--- index.js
+++ index.js
@@ -9,32 +9,34 @@ const f = () => {
         'headers': {
             'User-Agent': 'request'
         }
-
     }
     req.get('https://api.github.com/repos/' + config.user + '/' + config.repo + '/releases', options, (error, response, body) => {
-        body = JSON.parse(body)
-        var entities = []
-        body.forEach((release) => {
-            var entity = {}
-            entity.name = "Release: " + release.tag_name
-            entity.id = "Release ID: " + release.id
-            entity.date = "Release Date: " + release.created_at
-            entity.totalDownloads = 0
-            if (release.assets[0]) {
-                entity.downloads = []
-                release.assets.forEach((asset) => {
-                    var file = {}
-                    file.name = asset.name
-                    file.downloads = asset.download_count
-                    entity.totalDownloads += asset.download_count
-                    entity.downloads.push(file)
-                })
-                entities.push(entity)
-            }
-        })
-        fs.writeFile('./data.yaml', yaml.safeDump(entities), 'utf8', function(err, out) {
-            if(err) console.log(err)
-        })
+        if(!error) {
+            body = JSON.parse(body)
+            var entities = []
+            body.forEach((release) => {
+                var entity = {}
+                entity.name = "Release: " + release.tag_name
+                entity.id = "Release ID: " + release.id
+                entity.date = "Release Date: " + release.created_at
+                entity.totalDownloads = 0
+                if (release.assets[0]) {
+                    entity.downloads = []
+                    release.assets.forEach((asset) => {
+                        var file = {}
+                        file.name = asset.name
+                        file.downloads = asset.download_count
+                        entity.totalDownloads += asset.download_count
+                        entity.downloads.push(file)
+                    })
+                    entities.push(entity)
+                }
+            })
+            fs.writeFile('./data.yaml', yaml.safeDump(entities), 'utf8', function(err, out) {
+                if(err) console.log(err)
+            })
+        }
+        if (error) console.log("ERROR: " + error)
     });
 
 }
