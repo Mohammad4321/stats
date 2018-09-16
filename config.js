976a9cb18ec384505ab8e9f37918837e9fb6417d
hahnmichaelf <hahn.michael.f@gmail.com>
2018-09-16 19:47:19 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'user': 'mycryptohq',
+    'repo': 'mycrypto'
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const req = require('request')
+const fs = require('fs')
+const yaml = require('js-yaml')
+const config = require('./config.js')
+
+const f = () => {
+    const options = {
+        'method': 'GET',
+        'headers': {
+            'User-Agent': 'request'
+        }
+
+    }
+    req.get('https://api.github.com/repos/' + config.user + '/' + config.repo + '/releases', options, (error, response, body) => {
+        body = JSON.parse(body)
+        var entities = []
+        body.forEach((release) => {
+            var entity = {}
+            entity.name = "Release: " + release.tag_name
+            entity.id = "Release ID: " + release.id
+            entity.date = "Release Date: " + release.created_at
+            entity.totalDownloads = 0
+            if (release.assets[0]) {
+                entity.downloads = []
+                release.assets.forEach((asset) => {
+                    var file = {}
+                    file.name = asset.name
+                    file.downloads = asset.download_count
+                    entity.totalDownloads += asset.download_count
+                    entity.downloads.push(file)
+                })
+                entities.push(entity)
+            }
+        })
+        fs.writeFile('./data.yaml', yaml.safeDump(entities), 'utf8', function(err, out) {
+            if(err) console.log(err)
+        })
+    });
+
+}
+
+f();
